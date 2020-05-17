6f875d7cf7192c4975dd190179e36389510855f7
tylerpeller <49143891+tylerpeller@users.noreply.github.com>
2020-05-17 21:17:40 +0300

    Update Schwab_Scrape.py

--- Schwab_Scrape/Schwab_Scrape.py
+++ Schwab_Scrape/Schwab_Scrape.py
@@ -5,12 +5,12 @@ import csv
 
 
 #retrieve login info from file
-with open("c:/Users/tyler/OneDrive/Documents/Python_Scripts/Leumi.txt" ,"r") as login:
+with open("c:/Users/pathtofile/Schwab_Credentials.txt" ,"r") as login:
     user = login.readline()
     passwd = login.readline()
 
 
-driver = webdriver.Chrome('c:/Users/tyler/OneDrive/Documents/Python_Scripts/chromedriver')
+driver = webdriver.Chrome('c:/Users/pathtofile/chromedriver')
 driver.get('https://lms.schwab.com/Login/Full?clientId=schwab-mweb&mobile=yes&redirecturi=client.schwab.com/login/signon/authcodehandler.ashx&returnUrl=/')
 # frame = driver.find_element_by_css_selector('div.tool_forms iframe')
 # driver.switch_to.frame(frame)
@@ -18,12 +18,12 @@ t.sleep(5.5)
 
 #fill in username                       
 username = driver.find_element_by_id('LoginId')
-username.send_keys("user")
+username.send_keys(user)
 t.sleep(1.5)
 
 #fill in password
 password = driver.find_element_by_id('Password')
-password.send_keys("passwd")
+password.send_keys(passwd)
 password.send_keys(Keys.ENTER)
 
 email = driver.find_element_by_xpath("//input[@id='EmailId']")
